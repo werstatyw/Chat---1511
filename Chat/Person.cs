b634ba34503db4f0e839d2c146b0e8ce3ae6d418
hallgató <hallgat@LABOR115-01>
2017-11-15 12:33:42 +0100

    Add project files.

---
+++ Chat/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    enum Gender { male, female}
+    class Person
+    {
+        static string nameCorrection(string s)
+        {
+            return s.Substring(0, 1).ToUpper() + s.Substring(1).ToLower();
+        }//--
+        Random rand = new Random();
+        private string name;
+        public string FamilyName //HU way
+        {
+            get { return nameCorrection(name.Substring(0, name.IndexOf(" ")));  }
+            // set { name = value; }
+        }
+        public string GivenName //HU way
+        {
+            get { return nameCorrection(name.Substring(name.IndexOf(" ")).Trim()); }
+            // set { name = value; }
+        }
+        private int birthYear;
+        public int BirthYear
+        {
+            get { return birthYear; }
+            //set { birthYear = value; }
+        }
+        Gender g;
+        public Gender G
+        {
+            get { return g; }
+        }
+        public Person(string name, Gender g)
+        {
+            this.name = name.Trim();
+            this.g = g;
+            birthYear = rand.Next(1950, 2018);
+        }
+        public string print()
+        {
+            return "\n Given name: *" + GivenName + "* Family name: *" + FamilyName + "* Birth year: *" + BirthYear + "* Gender: *" + g + "*";
+        }
+    }
+}
---
+++ Chat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberofPersons = 3;
+            int ageDiff = 5;
+            Console.WriteLine("\nTask 1: generate data");
+            Person[] p = new Person[numberofPersons];
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.Write("Name of person " + i + " : ");
+                string s = Console.ReadLine();
+                Console.Write("Gender of person " + i + " (m/f): ");
+                string s1 = Console.ReadLine().ToLower();
+                if (s1[0] == 'f') { p[i] = new Person(s, Gender.female);}
+                else { p[i] = new Person(s, Gender.male);}
+            }//end for i
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.WriteLine(p[i].print());
+            }
+
+
+            Console.WriteLine("The end");
+            Console.ReadKey();
+        }
+    }
+}
